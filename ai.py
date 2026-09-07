0c88c14f06153fa4d5754cc56296b68014885375
mokagad <moka.gad7@gmail.com>
2026-09-07 15:16:42 +0300

    Update ai.py

--- ai.py
+++ ai.py
@@ -2,7 +2,7 @@
 # pip install google-genai
 
 from google import genai
-from google.genai import types
+from google.genai import types 
 
 def generate(description, instruction, api_key):
     client = genai.Client(api_key=api_key)
